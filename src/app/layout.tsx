56baca522b2923d283b0a293b399786d0e691c24
cwjhwhs <cwj.hwhs@gmail.com>
2026-05-07 19:48:34 +0800

    Delete src/app directory

--- src/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="zh">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  )
-}
-
